c83b7703e71a4d17b8eee3e99f855a2a8abb0deb
Zameena Abbas <farhana.z.abbas@gmail.com>
2022-01-20 19:43:36 -0500

    added a post request from front end to send blobs through formData

--- server/seeds/seeding_data.js
+++ server/seeds/seeding_data.js
@@ -7,21 +7,3 @@ exports.seed = function (knex) {
       return knex("portals").insert(portalsData);
     });
 };
-
-// /**
-//  * @param { import("knex").Knex } knex
-//  * @returns { Promise<void> }
-//  */
-// exports.seed = function(knex) {
-//   // Deletes ALL existing entries
-//   return knex('portals')
-//   .del()
-//     .then(function () {
-//       // Inserts seed entries
-//       return knex('portals').insert([
-//         {id: 1, colName: 'rowValue1'},
-//         {id: 2, colName: 'rowValue2'},
-//         {id: 3, colName: 'rowValue3'}
-//       ]);
-//     });
-// };

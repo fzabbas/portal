eca17066cb5f7ec51fe64ca31c87b32e05831695
Zameena Abbas <farhana.z.abbas@gmail.com>
2022-02-01 22:17:26 -0500

    added logs to all routes in server

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,6 +7,7 @@ const knex = require("../knexConfig");
 const Y = require("yjs");
 
 router.post("/", upload.single("portalDoc"), (req, res) => {
+  console.log("POST", req.body.password);
   knex("portals")
     .insert({
       portal_doc: req.file.buffer,
@@ -14,23 +15,28 @@ router.post("/", upload.single("portalDoc"), (req, res) => {
       password: req.body.password,
     })
     .then((data) => {
+      console.log("POST success");
       res.send("Portal was successfully created");
     })
     .catch((err) => {
+      console.log("POST failed", err);
       res.status(400).send("Could not make new portal");
     });
 });
 
 router.get("/:key", (req, res) => {
+  console.log("GET", req.params.key);
   knex("portals")
     .where({
       password: req.params.key,
     })
     .select("portal_doc")
     .then((data) => {
+      console.log("GET success");
       res.send(data[0].portal_doc);
     })
     .catch((err) => {
+      console.log("GET failed", err);
       res.status(400).send("error");
     });
 });
@@ -71,8 +77,8 @@ router.put("/:key", upload.single("portalDoc"), (req, res) => {
       console.log("successful put");
       res.send("portal was updates");
     })
-    .catch(() => {
-      console.log("Portal was not updated");
+    .catch((err) => {
+      console.log("Portal was not updated", err);
       res.status(400).send("Portal was not updated");
     });
 });
